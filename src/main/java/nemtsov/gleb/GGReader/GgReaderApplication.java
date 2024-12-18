96775bf67176b15af753b205c9161df627c743c4
Nemtsov Gleb <gnemtsov@systematica-consulting.ru>
2024-12-18 18:49:14 +1000

    create Reader

---
+++ src/main/java/nemtsov/gleb/GGReader/GgReaderApplication.java
@@ -0,0 +1,14 @@
+package nemtsov.gleb.GGReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgReaderApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/nemtsov/gleb/GGReader/GgReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package nemtsov.gleb.GGReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
